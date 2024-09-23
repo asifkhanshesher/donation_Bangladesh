b44f4e00f7c0ebf5d95c1eefba3bb5e26dc81fb8
Asif Khan <mdasifkhanshesher@gmail.com>
2024-09-24 01:29:55 +0600

    added donation button function

--- js/tabs.js
+++ js/tabs.js
@@ -1,10 +1,26 @@
 const historyTab = document.getElementById("history_tab");
 const donationTab = document.getElementById("donation_tab");
 
+
+// for history button
 historyTab.addEventListener("click", function () {
   historyTab.classList.add("bg-[#B4F461]", "rounded-md");
 
   donationTab.classList.remove("bg-[#B4F461]", "rounded-md");
 
-  document.getElementById('donation_cards').classList.add('hidden')
+  document.getElementById('donation_section').classList.add('hidden');
+  document.getElementById('history_section').classList.remove('hidden');
 });
+
+
+
+// for donation button
+donationTab.addEventListener('click',function(){
+  donationTab.classList.add("bg-[#B4F461]", "rounded-md");
+
+  historyTab.classList.remove("bg-[#B4F461]", "rounded-md");
+
+
+  document.getElementById('donation_section').classList.remove('hidden');
+  document.getElementById('history_section').classList.add('hidden')
+})
